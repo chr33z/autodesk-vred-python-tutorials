fa3f93b291d0344abec72c3a29a3dad97ab7dd76
Christopher Gebhardt <chris.geb@gmx.de>
2020-12-08 07:35:53 +0100

    Added material switch example

--- vred-pro/track-1/tutorial-3/variant_set_scripts.py
+++ vred-pro/track-1/tutorial-3/variant_set_scripts.py
@@ -1,5 +1,45 @@
 # Example 1
 # Recreating material switches
+print('--- Create Switch Materials ---')
+
+allMaterials = getAllMaterials()
+
+# get all materials which name starts with "switch"
+switchMaterials = list(filter(lambda matPtr: matPtr.getName().startswith('switch'), allMaterials))
+
+# iterate all switch materials and process
+for switchMaterial in switchMaterials:
+    switchPtr = switchMaterial
+    switchName = switchPtr.getName()
+    print('Found switch: ' + str(switchName))
+
+    # extract material prefix
+    materialPrefix = '_'.join(switchName.split('_')[1:])
+
+    # Get all materials which name starts with the material prefix 
+    childMaterials = list(filter(lambda matPtr: matPtr.getName().startswith(materialPrefix), allMaterials))
+    childMaterialNames = [x.getName() for x in childMaterials]
+    print('Found child materials: ' + ', '.join(childMaterialNames))
+
+    # Create new material switch
+    print('Create new material switch...')
+    newMaterialSwitch = createMaterial("SwitchMaterial")
+    newMaterialSwitch.setName(switchName)
+
+    # Add child materials to switch
+    for child in childMaterials:
+        newMaterialSwitch.addMaterial(child)
+
+    # swap old switch material with new switch material
+    print('Assign new material switch...')
+    nodes = switchPtr.getNodes()
+    for node in nodes:
+        node.setMaterial(newMaterialSwitch)
+
+    print('')
+
+print('Finished!')
+print('---')
 
 # Example 2
 # Calling global functions in the script editor
